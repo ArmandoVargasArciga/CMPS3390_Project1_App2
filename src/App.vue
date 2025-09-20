e0f83e578aefc48e4277cf0f7a9972e04501c464
Armando <vargas.a2004@gmail.com>
2025-09-20 16:42:12 -0700

    moved table to another page

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,9 @@
 </script>
 
 <template>
-<table>
-  <tr>
-    <th>Hobby</th>
-    <th>Date</th>
-  </tr>
-  <tr>
-    <td>Essay due</td>
-    <td>9/20/2025</td>
-  </tr>
-  <tr>
-    <td>Birthday</td>
-    <td>9/25/2025</td>
-  </tr>
-  <button @click="test">TEST</button>
-</table>
+
 </template>
 
 <style scoped>
-table{
-  width: 40%;
-  border:1px solid black;
-  margin: 50px auto;
-}
+
 </style>
---
+++ src/componets/habitTable.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+<table>
+  <tr>Habit List</tr>
+  <tr>
+    <th>Hobby</th>
+    <th>Date</th>
+  </tr>
+  <tr>
+    <td>Essay due</td>
+    <td>9/20/2025</td>
+  </tr>
+  <tr>
+    <td>Birthday</td>
+    <td>9/25/2025</td>
+  </tr>
+  <input type="text">
+  <button @click="test">TEST</button>
+</table>
+</template>
+
+<style scoped>
+table{
+  width: 40%;
+  height: 60%;
+  border:1px solid black;
+  margin: 50px auto;
+}
+th{
+  border:1px solid black;
+}
+</style>
